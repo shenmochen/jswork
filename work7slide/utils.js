7f614a9bd8bf73c1977aa69dc9613debc1eec069
shenmochen <1847190586@qq.com>
2019-10-21 12:04:11 +0800

    work7slide

---
+++ work7slide/utils.js
@@ -0,0 +1,34 @@
+function bubbleSort(str) {
+    let arr = str.split(',')
+    let sortLog = []
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            sortLog.push([arr.concat(), [j, j + 1]])
+            sortLog.push([arr.concat(), [j, j + 1]])
+            sortLog.push([arr.concat(), [j, j + 1]])
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+                sortLog.push([arr.concat(), [j, j + 1]])
+            }
+        }
+    }
+    sortLog.push([arr.concat(), [-1, -1]])
+    return sortLog
+}
+function showLog(logValues, showElement) {
+    let str = ''
+    let {
+        done,
+        value: [row, pos]
+    } = logValues.next()
+    for (const key in row) {
+        let color = pos.indexOf(Number(key)) > -1 ? 'color:red;' : ''
+        str += '<span style="font-size:' + row[key] * 20 + 'px;' +
+                color + '">' + row[key] + '</span>'
+    }
+    showElement.innerHTML = str
+    if (pos[0] != -1)
+        setTimeout("showLog(logValues,showDiv)", 500)
+}
+function insertSort(str) {
+}
