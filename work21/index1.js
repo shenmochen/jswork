0efdaea4e9db8bbbccacf878e4a88b1574698eb5
shenmochen <1847190586@qq.com>
2020-01-02 16:20:11 +0800

    work21

--- work21/index1.js
+++ work21/index1.js
@@ -1,5 +1,5 @@
 $("input:last").click(function(){
-    $.post("/ajax",$(form).serialize(),function(res){
+    $.post("/ajax",$('form').serialize(),function(res){
         result =res.reverse().map((val)=>{return JSON.stringify(val)+"<br>"})
         $("#result").empty().append($("<div></div>").html(result))
     })
