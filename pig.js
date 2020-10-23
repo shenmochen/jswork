6dee90ac5ec70ce6949a4a02cf7323cfe4ba8a89
shenmochen <1847190586@qq.com>
2020-10-23 22:46:14 +0800

    pig.js

--- pig.js
+++ pig.js
@@ -5,54 +5,56 @@ const child_process = require('child_process')
 const querystring = require('querystring')
 const readline = require('readline')
 const iconv = require('iconv-lite')
-const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require('constants')
 const rl = readline.createInterface({
     input:process.stdin
 })
 let buf=[]
 rl.on('line',(line)=>{
-    if(line.trim()=='') rl.close()
+    if(line.trim()=='')rl.close()
     buf.push(line)
 })
 rl.on('close',()=>{
     vcode = buf[0].trim()
-    console.log('验证码是: ',vcode)
+    console.log('验证码：',vcode)
     login()
 })
 let {
     user,
     password
-} = JSON.parse(require('fs').readFileSync('zf.json', 'utf8'))
-
+} = JSON.parse(require('fs').readFileSync('zf.json','utf-8'))
 let baseurl = 'https://jwc.gdmec.edu.cn'
 let vcodeurl = 'https://jwc.gdmec.edu.cn/CheckCode.aspx'
-let someurl = 'https://jwc.gdmec.edu.cn/xs_main.aspx?xh=07190527'
+let someurl = 'https://jwc.gdmec.edu.cn/xsxkqk.aspx?xh=07190527&xm=%C1%F5%C3%CE%D5%E6&gnmkdm=N121615'
 let __VIEWSTATE
-let vcode, cookie
-https.get(baseurl, (res) => {
+let vcode,cookie
+https.get(baseurl,(res)=>{
     let chunks = []
-    require,on('data', (chunk) => {
+    res.on('data',(chunk)=>{
         chunks.push(chunk)
     })
+    res.on('end',()=>{
+        let $ = cheerio.load(chunks.toString())
+        __VIEWSTATE = $('input[name="__VIEWSTATE"]')[0].attribs.value
+        console.log(__VIEWSTATE)
+    })
 })
-
-https.get(vcodeurl, (res) => {
+https.get(vcodeurl,(res)=>{
     cookie = res.headers['set-cookie']
     res.setEncoding('binary')
-    let imgData =''
+    let imgData = ''
     res.on('data',(chunk)=>{
         imgData += chunk
     })
     res.on('end',()=>{
         fs.writeFile('vcode.png',imgData,'binary',(err)=>{
             if(err){
-                console.log(err)
+               console.log(err)
                 return
             }
             if(process.platform=='win32')
-                child_process.exec('vcode.png')
-                else
-                child_process.exec('open vcode.png')
+            child_process.exec('vcode.png')
+            else
+            child_process.exec('open vcode.png')
         })
     })
 })
@@ -63,7 +65,7 @@ function login(){
         TextBox2:password,
         TextBox3:vcode,
         Button1:'',
-        ReadioButtonList1:''
+        RadioButtonList1:''
     })
     postData += '%D1%A7%C9%FA'
     console.log(postData)
@@ -74,30 +76,30 @@ function login(){
         method:'post',
         headers:{
             'content-type':'application/x-www-form-urlencoded',
-            'content-type':Buffer.byteLength(postData),
+            'content-length':Buffer.byteLength(postData),
             'Cookie':cookie
         }
     }
-let req = https.request(opt,(res)=>{
-    let chunks =[]
-    res.on('data',(chunk)=>{
-        chunks.push(chunk)
+    let req = https.request(opt,(res)=>{
+        let chunks =[]
+        res.on('data',(chunk)=>{
+            chunks.push(chunk)
+        })
+        res.on('end',()=>{
+           console.log(chunks.toString())
+            geturl()
+        })
     })
-    res.on('end',()=>{
-        console.log(chunks.toString())
-        geturl()
-    })
-})
-   req.write(postData)
-   req.end()
+    req.write(postData)
+    req.end()
 }
 function geturl(){
-    let refer = `https://jwc.gdmec.edu.cn/xs_main.aspx?xh=07190527`
-    let opt={
-        headers:{
-            'Referer':refer,
-            'Cookie':cookie,
-        }
+    let refer = `https://jwc.gdmec.edu.cn/js_main.aspx?xh=${user}`
+    let opt = {
+    headers:{
+        'Referer':refer,
+        'Cookie':cookie,
+    }
     }
     https.get(someurl,opt,(res)=>{
         let chunks=[]
@@ -106,9 +108,9 @@ function geturl(){
         })
         res.on('end',()=>{
             let buffer = Buffer.concat(chunks)
-            let str = iconv.decode(buffer,'gbk')
+            let str =iconv.decode(buffer,'gbk')
             let $ = cheerio.load(str)
-            $('#DataGrid1>tbody>tr>td:nth-child(3)').map(function(el){
+            $('#kcmcgrid>tbody>tr>td:nth-child(2)').map(function(el){
                 console.log($(this).text())
             })
         })
